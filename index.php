224280e80e3a38fb34841e7916c8b2bba2945ad3
vyshnavimudumby <vyshnavimudumby@gmail.com>
2018-11-09 00:04:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+// esp
+// /
+// if(status not =)
+	// respomse = new status
+// else
+	// response = no update
+
+
+// gh
+// /?light=*
+// dbc= set status = $light
+
+
+<?php
+define('DB_HOST', 'jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+define('DB_USER', 'b8wib9i2mk7g6ejb');
+define('DB_PASSWORD', 'oxea6npqrd3ujrly');
+define('DB_NAME', 'mmub67bw1c8vermf');
+$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
+
+
+ if(isset($_GET['light'])){
+	
+ }else{
+	$query = "SELECT * FROM device_values";
+	$data = mysqli_query($dbc, $query);
+	$row = mysqli_fetch_array($data)
+	echo $row['is_active']; 
+ }
+mysqli_close($dbc); 
+?>
